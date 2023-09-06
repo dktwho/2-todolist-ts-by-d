00b5b5ef4617adbca7cb059bc64cff9295b12101
Alexander Korobov <dktwho@mail.ru>
2023-09-06 14:44:34 +0300

    modified main.ts stories for addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,28 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+export default {
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
-  staticDirs: ["../public"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-onboarding",
+        "@storybook/addon-interactions",
+        {
+            name: '@storybook/addon-storysource',
+            options: {
+                rule: {
+                    test: [/\.stories\.jsx?$/],
+                },
+                loaderOptions: {
+                    prettierConfig: {printWidth: 80, singleQuote: false},
+                    options: {parser: 'typescript'}
+                },
+            },
+        },
+    ],
+
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {}
+    }
 };
-export default config;
